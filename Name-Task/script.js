faba5540acb4c75766ac67def388ecfe855c3df2
Dorcas-BD <52034693+Dorcas-BD@users.noreply.github.com>
2021-08-17 17:22:08 +0100

    initial commit

---
+++ Name-Task/script.js
@@ -0,0 +1,5 @@
+let firstName = "Dorcas";
+let lastName = "Bamisile";
+
+console.log("My name is " + firstName + " " + lastName)
+alert("Welcome to my page. My name is " + firstName + " " + lastName)
